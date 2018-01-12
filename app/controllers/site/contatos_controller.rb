6d2a3ae85138adf6d14edadfbc2edf99cd1fa721
Leonardo Ribas de Souza <leonardodsouza@gmail.com>
2018-01-12 19:26:39 -0200

    first commit

--- app/controllers/site/contatos_controller.rb
+++ app/controllers/site/contatos_controller.rb
@@ -6,7 +6,7 @@ class Site::ContatosController < ApplicationController
     end
 
     def enviar_contato
-        EmailMailer.contato(params[:nome],params[:email],params[:telefone],params[:mensagem]).deliver_now        
+        #EmailMailer.contato(params[:nome],params[:email],params[:telefone],params[:mensagem]).deliver_now        
         redirect_to site_contato_url, notice: 'Mensagem enviada com sucesso.'
     end
     
