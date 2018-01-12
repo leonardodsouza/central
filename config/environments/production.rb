7c71c33b0d18ae61c09ccae0d8ab5f24afda39c7
Leonardo Ribas de Souza <leonardodsouza@gmail.com>
2018-01-12 19:36:34 -0200

    first commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,4 +89,15 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :enable_starttls_auto => true,
+    :port => 587,
+    :authentication => :plain,
+    :user_name => "centraldeveiculossr@gmail.com",
+    :password => 'central@2015'
+  }
+
 end
