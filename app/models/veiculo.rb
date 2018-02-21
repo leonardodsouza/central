61665fd131640dea03086c7342072522b14fc173
Leonardo Ribas de Souza <leonardodsouza@gmail.com>
2018-02-21 10:09:29 -0300

    first commit

--- app/models/veiculo.rb
+++ app/models/veiculo.rb
@@ -1,5 +1,6 @@
 class Veiculo < ApplicationRecord
     has_many :galerias, dependent: :destroy
+    has_many :graficos, dependent: :destroy
     has_attached_file :foto, styles: {grande: "700X700>", capa: "600X600>", medium: "300x300#", thumb: "100x100#" }, default_url: "/images/:style/missing.png"
     validates_attachment_content_type :foto, content_type: /\Aimage\/.*\z/
     validates_presence_of :foto, :categoria, :preco, :marca, :modelo, :descricao
